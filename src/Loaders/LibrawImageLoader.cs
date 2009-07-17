2131a2cb63aea7a8ee6ad692ba5159a9b8c83564
Ruben Vermeersch <ruben@savanne.be>
2009-07-17 23:05:59 +0200

    Add stub interface for LibrawImageLoader.

--- src/Loaders/LibrawImageLoader.cs
+++ src/Loaders/LibrawImageLoader.cs
@@ -9,11 +9,38 @@
 // This is free software. See COPYING for details
 //
 
+using Gdk;
+using System;
+using FSpot.Utils;
 using FSpot.Loaders.Native;
 
 namespace FSpot.Loaders {
-	public class LibrawImageLoader {
-		NativeLibrawLoader loader;
+	public class LibrawImageLoader : IImageLoader {
 
+		public void Load (Uri uri)
+		{
+
+		}
+
+		public event EventHandler<AreaPreparedEventArgs> AreaPrepared;
+		public event EventHandler<AreaUpdatedEventArgs> AreaUpdated;
+		public event EventHandler Completed;
+
+		public bool Loading {
+			get { throw new Exception ("Not implemented yet!"); }
+		}
+
+		public void Dispose ()
+		{
+
+		}
+
+		public Pixbuf Pixbuf {
+			get { throw new Exception ("Not implemented yet!"); }
+		}
+
+		public PixbufOrientation PixbufOrientation {
+			get { throw new Exception ("Not implemented yet!"); }
+		}
 	}
 }
