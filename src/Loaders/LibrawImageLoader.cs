242665523b1f2f6698a03de1828e5ca9df0e2b8a
Ruben Vermeersch <ruben@savanne.be>
2009-07-18 11:17:01 +0200

    Add libfspotraw-sharp as a build dependency.

---
+++ src/Loaders/LibrawImageLoader.cs
@@ -0,0 +1,19 @@
+//
+// Fspot.Loaders.LibrawImageLoader.cs
+//
+// Copyright (c) 2009 Novell, Inc.
+//
+// Author(s)
+//	Ruben Vermeersch  <ruben@savanne.be>
+//
+// This is free software. See COPYING for details
+//
+
+using FSpot.Loaders.Native;
+
+namespace FSpot.Loaders {
+	public class LibrawImageLoader {
+		NativeLibrawLoader loader;
+
+	}
+}
