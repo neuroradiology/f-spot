646a53a30e8e6bc57ccc1499fff5ac1bf0882aa7
Ruben Vermeersch <ruben@savanne.be>
2010-06-20 14:34:46 +0200

    Let PhotoLoaderCache use ILoadable as indices.

--- src/Loaders/PhotoLoaderCache.cs
+++ src/Loaders/PhotoLoaderCache.cs
@@ -5,24 +5,25 @@ namespace FSpot.Loaders
 {
     public class PhotoLoaderCache
     {
-        Dictionary<string, WeakReference> Loaders = new Dictionary<string, WeakReference>();
+        Dictionary<ILoadable, WeakReference> Loaders = new Dictionary<ILoadable, WeakReference>();
 
         public IPhotoLoader RequestLoader (ILoadable photo)
         {
-            var key = photo.Uri.ToString ();
             WeakReference reference = null;
             IPhotoLoader loader = null;
 
-            if (Loaders.TryGetValue (key, out reference)) {
+            if (Loaders.TryGetValue (photo, out reference)) {
                 loader = reference.Target as IPhotoLoader;
             }
 
             if (loader == null) {
                 loader = new XdgThumbnailLoader (photo);
-                Loaders[key] = new WeakReference (loader);
+                Loaders[photo] = new WeakReference (loader);
             }
 
             return loader;
         }
+
+		// FIXME: periodically purge
     }
 }
